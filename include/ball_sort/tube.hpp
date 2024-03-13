b85bedacc4ae4de9232136feedad242836849309
David Jordan <le_dangereux@hotmail.co.uk>
2024-03-13 23:09:06 +0000

    Extract puzzle data and solver into separate classes

--- include/ball_sort/tube.hpp
+++ include/ball_sort/tube.hpp
@@ -18,10 +18,10 @@ class Tube {
     auto get_top_ball() const -> char;
     auto get_serialised_balls() const -> std::string;
     auto take_top_ball() -> char;
-    auto place_ball(char ball) -> void;
+    auto place_ball(const char ball) -> void;
 
     static const size_t MAX_CAPACITY{4};
 
  private:
-    std::string balls;
+    std::string m_balls;
 };
