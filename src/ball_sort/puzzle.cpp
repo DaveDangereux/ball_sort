eeccb8ff48f35b85320cc293c9b679975f0cd0f2
David Jordan <le_dangereux@hotmail.co.uk>
2024-03-14 17:18:22 +0000

    Divide move validation and move selection between puzzle and solver

--- src/ball_sort/puzzle.cpp
+++ src/ball_sort/puzzle.cpp
@@ -50,64 +50,6 @@ auto Puzzle::make_tubes(const std::string &number_string) -> std::vector<Tube>
     return tubes;
 }
 
-auto Puzzle::get_tubes() const -> const std::vector<Tube> &
-{
-    return m_tubes;
-}
-
-auto Puzzle::do_move(const Move move) -> void
-{
-    char ball{m_tubes.at(move.origin).take_top_ball()};
-    m_tubes.at(move.destination).place_ball(ball);
-    m_history.push_back(move);
-
-    if (m_previous_puzzle_states.contains(get_serialised_tubes())) {
-        undo_move();
-    } else {
-        m_previous_puzzle_states.insert(get_serialised_tubes());
-    }
-}
-
-auto Puzzle::reset() -> void
-{
-    m_tubes = m_initial_state;
-    m_history.clear();
-}
-
-auto Puzzle::get_ball_tally() const -> std::unordered_map<char, size_t>
-{
-    std::unordered_map<char, size_t> ball_tally{};
-
-    for (const Tube &tube : m_tubes) {
-        for (const char &ball : tube.get_balls()) {
-            ball_tally[ball]++;
-        }
-    }
-
-    return ball_tally;
-}
-
-auto Puzzle::get_serialised_tubes() const -> std::string
-{
-    std::string serialised_tubes{};
-
-    for (const Tube &tube : m_tubes) {
-        serialised_tubes.append(tube.get_serialised_balls());
-    }
-
-    return serialised_tubes;
-}
-
-auto Puzzle::is_solved() const -> bool
-{
-    for (const Tube &tube : m_tubes) {
-        if (tube.is_empty()) continue;
-        if (!tube.is_solved()) return false;
-    }
-
-    return true;
-}
-
 auto Puzzle::validate_puzzle() const -> void
 {
     std::unordered_map<char, size_t> ball_tally{get_ball_tally()};
@@ -128,14 +70,36 @@ auto Puzzle::validate_puzzle() const -> void
     }
 }
 
-auto Puzzle::get_excluded_moves() const -> const std::unordered_set<Move> &
+auto Puzzle::get_legal_moves() const -> std::vector<Move>
 {
-    return m_excluded_moves;
+    std::vector<Move> legal_moves{};
+    std::string current_state = get_serialised_state();
+
+    for (size_t o{0}; o < m_tubes.size(); ++o) {
+        for (size_t d{0}; d < m_tubes.size(); ++d) {
+            if (is_legal_move(o, d))
+                legal_moves.push_back(Move{current_state, o, d});
+        }
+    }
+
+    return legal_moves;
 }
 
-auto Puzzle::get_history() const -> const std::vector<Move> &
+auto Puzzle::do_move(const size_t origin, const size_t destination) -> void
 {
-    return m_history;
+    m_history.push_back(Move{get_serialised_state(), origin, destination});
+
+    char ball{m_tubes.at(origin).take_top_ball()};
+    m_tubes.at(destination).place_ball(ball);
+
+    std::string current_state{get_serialised_state()};
+
+    if (m_previous_puzzle_states.contains(current_state)) {
+        m_is_novel_puzzle_state = false;
+    } else {
+        m_previous_puzzle_states.insert(current_state);
+        m_is_novel_puzzle_state = true;
+    }
 }
 
 auto Puzzle::undo_move() -> void
@@ -145,6 +109,76 @@ auto Puzzle::undo_move() -> void
     char ball{m_tubes.at(last_move.destination).take_top_ball()};
     m_tubes.at(last_move.origin).place_ball(ball);
 
-    m_excluded_moves.insert(last_move);
+    m_is_novel_puzzle_state = false;
     m_history.pop_back();
+}
+
+auto Puzzle::is_novel_puzzle_state() const -> bool
+{
+    return m_is_novel_puzzle_state;
+}
+
+auto Puzzle::is_solved() const -> bool
+{
+    for (const Tube &tube : m_tubes) {
+        if (tube.is_empty()) continue;
+        if (!tube.is_solved()) return false;
+    }
+
+    return true;
+}
+
+auto Puzzle::reset() -> void
+{
+    m_tubes = m_initial_state;
+    m_history.clear();
+    m_previous_puzzle_states.clear();
+}
+
+auto Puzzle::get_tubes() const -> const std::vector<Tube> &
+{
+    return m_tubes;
+}
+
+auto Puzzle::get_history() const -> const std::vector<Move> &
+{
+    return m_history;
+}
+
+auto Puzzle::is_legal_move(const size_t origin_index,
+                           const size_t destination_index) const -> bool
+{
+    const Tube origin{m_tubes.at(origin_index)};
+    const Tube destination{m_tubes.at(destination_index)};
+
+    if (origin_index == destination_index) return false;
+    if (origin.is_empty()) return false;
+    if (destination.is_full()) return false;
+
+    return (origin.get_top_ball() == destination.get_top_ball()) ||
+           destination.is_empty();
+}
+
+auto Puzzle::get_ball_tally() const -> std::unordered_map<char, size_t>
+{
+    std::unordered_map<char, size_t> ball_tally{};
+
+    for (const Tube &tube : m_tubes) {
+        for (const char &ball : tube.get_balls()) {
+            ball_tally[ball]++;
+        }
+    }
+
+    return ball_tally;
+}
+
+auto Puzzle::get_serialised_state() const -> std::string
+{
+    std::string serialised_tubes{};
+
+    for (const Tube &tube : m_tubes) {
+        serialised_tubes.append(tube.get_serialised_balls());
+    }
+
+    return serialised_tubes;
 }
