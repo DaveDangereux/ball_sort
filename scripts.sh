01d44d08b77b036e76d636f632629fe80fc6a17f
David Jordan <le_dangereux@hotmail.co.uk>
2024-03-15 16:58:00 +0000

    Remove all references to nodemon and tmux from scripts

--- scripts.sh
+++ scripts.sh
@@ -3,10 +3,6 @@
 project_name=ball_sort
 test_executable=ball_sort_tests
 
-god_mode_width=50%
-test_pane_height=75%
-cmake_pane_width=20%
-
 if [ $# -eq 0 ]; then
 	echo "Usage: $0 <command>"
 	exit 1
@@ -24,30 +20,8 @@ case "$command" in
 "build-tests")
 	cmake --build build --target $test_executable
 	;;
-"watch-cmake")
-	npx nodemon -e txt --ignore ./build -x "./scripts.sh build-cmake || exit 1"
-	;;
-"watch-main")
-	npx nodemon -e cpp,hpp --ignore ./build -x "clear && ./scripts.sh build-main && ./bin/$project_name || exit 1"
-	;;
-"watch-tests")
-	npx nodemon -e cpp,hpp --ignore ./build -x "clear && ./scripts.sh build-tests && ./bin/$test_executable || exit 1"
-	;;
 "clean")
 	rm -rf build
-	;;
-"god-mode")
-	./scripts.sh build-cmake
-	tmux resize-pane -x $god_mode_width
-	tmux split-window -v
-	tmux resize-pane -y $test_pane_height
-	tmux select-pane -t -
-	tmux split-window -h
-	tmux resize-pane -x $cmake_pane_width
-	tmux send-keys -t - "./scripts.sh watch-main" Enter
-	tmux select-pane -t +
-	tmux send-keys -t - "./scripts.sh watch-cmake" Enter
-	tmux send-keys "./scripts.sh watch-tests" Enter
 	;;
 esac
 
