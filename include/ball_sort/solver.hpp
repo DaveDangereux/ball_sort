b383ea8dde35dd036885f0ffa87a17198c34df11
David Jordan <le_dangereux@hotmail.co.uk>
2024-03-22 21:15:34 +0000

    Make pick_move and generate_filtered_moves private
    In Solver, make pick_move and generate_filtered_moves (formerly
    purge_redundant_moves) private methods. Update Solver tests to test
    generate_filtered_moves via the public interface.

--- include/ball_sort/solver.hpp
+++ include/ball_sort/solver.hpp
@@ -9,10 +9,10 @@ class Solver {
     static void print_puzzle(const Puzzle& puzzle);
     static void play_solution(Puzzle& puzzle, size_t moves_per_second);
 
-    [[nodiscard]] static Move
-    pick_move(const std::vector<Move>& filtered_moves);
-
+ private:
     [[nodiscard]] static std::vector<Move>
     generate_filtered_moves(const Puzzle& puzzle,
                             const std::unordered_set<Move>& excluded_moves);
+    [[nodiscard]] static Move
+    pick_move(const std::vector<Move>& filtered_moves);
 };
