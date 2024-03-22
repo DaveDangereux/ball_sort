acc595bb42bc15edcc45d787c7d8831dfa51860e
David Jordan <le_dangereux@hotmail.co.uk>
2024-03-22 21:15:34 +0000

    Make pick_move and generate_filtered_moves private
    In Solver, make pick_move and generate_filtered_moves (formerly
    purge_redundant_moves) private methods. Update Solver tests to test
    generate_filtered_moves via the public interface.

--- tests/test_solver.cpp
+++ tests/test_solver.cpp
@@ -1,12 +1,9 @@
-#include "ball_sort/move.hpp"
 #include "ball_sort/solver.hpp"
 #include <gtest/gtest.h>
 
 TEST(SolverTest, AllSolvedTubesMeansNoMoves)
 {
     Puzzle puzzle{{"AAAA", "BBBB", "", ""}};
-    std::vector<Move> legal_moves{puzzle.generate_legal_moves()};
-    std::vector<Move> filtered_moves{
-        Solver::generate_filtered_moves(puzzle, {})};
-    EXPECT_TRUE(filtered_moves.empty());
+    Solver::solve(puzzle, false);
+    EXPECT_TRUE(puzzle.get_history().empty());
 }
