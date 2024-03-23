df0be5ade7dbe139a1dd763995e2d328fec6c787
David Jordan <le_dangereux@hotmail.co.uk>
2024-03-23 00:01:41 +0000

    Add default for serialised_state in Move constructor
    Closes DT-32

--- tests/test_move.cpp
+++ tests/test_move.cpp
@@ -5,7 +5,7 @@ TEST(MoveTest, OriginAndDestinationAreCorrectWhenConstructed)
 {
     size_t origin{0};
     size_t destination{1};
-    Move move{std::pair{origin, destination}, ""};
+    Move move{std::pair{origin, destination}};
     EXPECT_EQ(move.get_origin(), origin);
     EXPECT_EQ(move.get_destination(), destination);
 }
