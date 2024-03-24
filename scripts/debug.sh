31fbde98e0fb2757240c011d8389474499ebf6bb
David Jordan <le_dangereux@hotmail.co.uk>
2024-03-24 00:08:22 +0000

    Add address and undefined behaviour sanitisers
    Address and undefined behaviour sanitisers are on by default for debug
    builds.

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-safe() {
-	$@
-	if [ $? -ne 0 ]; then
-		echo "Build error"
-		exit 1
-	fi
-}
+source $(dirname "$0)")/safe.sh
 
 rm -rf build
 safe conan install . -s build_type=Debug --build=missing -s compiler.cppstd=20
-safe cmake -DENABLE_WARNINGS=ON -DENABLE_WARNINGS_AS_ERRORS=ON --preset conan-debug
+safe cmake -DENABLE_SANITISE_ADDRESS=ON -DENABLE_SANITISE_UNDEFINED=ON --preset conan-debug
 safe cmake --build --preset conan-debug
