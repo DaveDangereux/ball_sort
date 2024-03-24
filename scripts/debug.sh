8ed5fff30416a3a26e1d0e4a77c2cad5c7d2d475
David Jordan <le_dangereux@hotmail.co.uk>
2024-03-24 18:56:44 +0000

    Remove safe.sh and replace with set -e in scripts

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-source $(dirname "$0)")/safe.sh
+set -e
 
 rm -rf build
-safe conan install . -s build_type=Debug --build=missing -s compiler.cppstd=20
-safe cmake -DENABLE_SANITISE_ADDRESS=ON -DENABLE_SANITISE_UNDEFINED=ON --preset conan-debug
-safe cmake --build --preset conan-debug
+conan install . -s build_type=Debug --build=missing -s compiler.cppstd=20
+cmake -DENABLE_SANITISE_ADDRESS=ON -DENABLE_SANITISE_UNDEFINED=ON --preset conan-debug
+cmake --build --preset conan-debug
