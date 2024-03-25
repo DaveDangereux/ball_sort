fde0e666d212e1ce7bd134bcc1dc67e54612810f
David Jordan <le_dangereux@hotmail.co.uk>
2024-03-25 00:59:31 +0000

    Convert Solver class to solver namespace

--- app/app.cpp
+++ app/app.cpp
@@ -8,7 +8,7 @@ int main()
     Puzzle puzzle{"9 10 1 5 2 12 8 12 10 4 6 10 8 5 7 1 10 11 3 7 5 4 1 2 3 8 "
                   "1 9 2 7 12 8 4 9 11 6 6 12 2 3 6 3 11 9 5 4 7 11"};
 
-    Solver::solve(puzzle, true);
+    solver::solve(puzzle, true);
 
     fmt::print("Play solution?\n");
     fmt::print("> ");
@@ -20,7 +20,7 @@ int main()
     const size_t moves_per_second{9};
 
     if (std::toupper(answer) == 'Y') {
-        Solver::play_solution(puzzle, moves_per_second);
+        solver::play_solution(puzzle, moves_per_second);
     }
 
     return 0;
