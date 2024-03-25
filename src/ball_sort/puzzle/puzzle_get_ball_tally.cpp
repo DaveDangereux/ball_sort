1f2d371f8fdb178c1a16b2d0b9e75a911bdcee8c
David Jordan <le_dangereux@hotmail.co.uk>
2024-03-25 17:51:44 +0000

    Reserve for unordered_map in Puzzle::get_ball_tally()

--- src/ball_sort/puzzle/puzzle_get_ball_tally.cpp
+++ src/ball_sort/puzzle/puzzle_get_ball_tally.cpp
@@ -2,7 +2,9 @@
 
 std::unordered_map<char, size_t> Puzzle::get_ball_tally() const
 {
+    const size_t letters_in_alphabet{26};
     std::unordered_map<char, size_t> ball_tally{};
+    ball_tally.reserve(letters_in_alphabet);
 
     for (const Tube& tube : m_tubes) {
         for (const char ball : tube.get_balls()) {
