6d881af9da638161cb163258b79229dd3eb7b231
David Jordan <le_dangereux@hotmail.co.uk>
2024-03-25 00:59:31 +0000

    Convert Solver class to solver namespace

--- include/ball_sort/solver.hpp
+++ include/ball_sort/solver.hpp
@@ -1,18 +1,17 @@
 #pragma once
 
-#include "ball_sort/move.hpp"
 #include "ball_sort/puzzle.hpp"
 
-class Solver {
- public:
-    static void solve(Puzzle& puzzle, bool display = false);
-    static void print_puzzle(const Puzzle& puzzle);
-    static void play_solution(Puzzle& puzzle, size_t moves_per_second);
+namespace solver {
 
- private:
-    [[nodiscard]] static std::vector<Move>
-    generate_filtered_moves(const Puzzle& puzzle,
-                            const std::unordered_set<Move>& excluded_moves);
-    [[nodiscard]] static Move
-    pick_move(const std::vector<Move>& filtered_moves);
-};
+void solve(Puzzle& puzzle, bool display = false);
+
+[[nodiscard]] std::vector<Move>
+generate_filtered_moves(const Puzzle& puzzle,
+                        const std::unordered_set<Move>& excluded_moves);
+
+[[nodiscard]] Move pick_move(const std::vector<Move>& filtered_moves);
+void print_puzzle(const Puzzle& puzzle);
+void play_solution(Puzzle& puzzle, size_t moves_per_second);
+
+} // namespace solver
