eb050943d92b3ff6e1e2b9e856d8f44daf1eef73
David Jordan <le_dangereux@hotmail.co.uk>
2024-03-25 02:44:12 +0000

    Break up Puzzle implementation into smaller files

---
+++ src/ball_sort/puzzle/puzzle.cpp
@@ -0,0 +1,56 @@
+#include "ball_sort/puzzle.hpp"
+#include "ball_sort/tube.hpp"
+#include <fmt/core.h>
+
+Puzzle::Puzzle(const std::vector<std::string>& letter_strings)
+    : m_initial_state{make_tubes(letter_strings)}, m_tubes{m_initial_state}
+{
+    validate_puzzle();
+}
+
+Puzzle::Puzzle(const std::string& number_string)
+    : m_initial_state{make_tubes(number_string)}, m_tubes{m_initial_state}
+{
+    validate_puzzle();
+}
+
+bool Puzzle::is_novel_puzzle_state() const
+{
+    return m_is_novel_puzzle_state;
+}
+
+bool Puzzle::is_solved() const
+{
+    return std::ranges::all_of(m_tubes, [](const Tube& tube) {
+        return tube.is_empty() || tube.is_solved();
+    });
+}
+
+void Puzzle::reset()
+{
+    m_tubes = m_initial_state;
+    m_move_history.clear();
+    m_visited_puzzle_states.clear();
+}
+
+const std::vector<Tube>& Puzzle::get_tubes() const
+{
+    return m_tubes;
+}
+
+const std::vector<Move>& Puzzle::get_history() const
+{
+    return m_move_history;
+}
+
+std::string Puzzle::get_serialised_state() const
+{
+    std::string serialised_state{};
+    serialised_state.reserve(m_tubes.size() * Tube::get_max_capacity());
+
+    for (const Tube& tube : m_tubes) {
+        serialised_state.append(tube.get_serialised_balls());
+    }
+
+    return serialised_state;
+}
---
+++ src/ball_sort/puzzle/puzzle_make_tubes.cpp
@@ -0,0 +1,61 @@
+#include "ball_sort/exceptions/illegal_puzzle_exception.hpp"
+#include "ball_sort/puzzle.hpp"
+#include <fmt/core.h>
+#include <sstream>
+
+std::vector<Tube>
+Puzzle::make_tubes(const std::vector<std::string>& letter_strings)
+{
+    auto make_tube_from_string{
+        [](const std::string& letter_string) { return Tube(letter_string); }};
+
+    std::vector<Tube> tubes{};
+    tubes.reserve(letter_strings.size());
+
+    std::transform(letter_strings.begin(), letter_strings.end(),
+                   std::back_inserter(tubes), make_tube_from_string);
+    return tubes;
+}
+
+std::vector<Tube> Puzzle::make_tubes(const std::string& number_string)
+{
+    std::istringstream number_stream{number_string};
+    std::vector<int> numbers{std::istream_iterator<int>{number_stream},
+                             std::istream_iterator<int>{}};
+
+    if (numbers.size() % Tube::get_max_capacity() != 0) {
+        throw IllegalPuzzleException(
+            fmt::format("Tried to make puzzle with {} balls, but need an "
+                        "integer multiple of tube capacity {}",
+                        numbers.size(), Tube::get_max_capacity()));
+    }
+
+    auto letter_from_number = [](int n) -> char {
+        return static_cast<char>('A' + n - 1);
+    };
+
+    const int MIN_NUMBER{1};
+    const int MAX_NUMBER{26};
+
+    std::string tube_string{};
+    std::vector<Tube> tubes{};
+    tubes.reserve(numbers.size() / 4);
+
+    for (int number : numbers) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw IllegalPuzzleException(
+                fmt::format("Number out of range: {}", number));
+        }
+
+        tube_string.push_back(letter_from_number(number));
+        if (tube_string.size() == Tube::get_max_capacity()) {
+            tubes.emplace_back(tube_string);
+            tube_string.clear();
+        }
+    }
+
+    tubes.emplace_back("");
+    tubes.emplace_back("");
+
+    return tubes;
+}
