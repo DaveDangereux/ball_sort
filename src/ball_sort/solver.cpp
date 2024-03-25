3d5f34af620688b26e963184f2ae8ba2f5d79624
David Jordan <le_dangereux@hotmail.co.uk>
2024-03-25 20:05:41 +0000

    Add const to estimated_excluded_move_count in solver::solve

--- src/ball_sort/solver.cpp
+++ src/ball_sort/solver.cpp
@@ -11,7 +11,7 @@ void solver::solve(Puzzle& puzzle, bool display)
 
     if (display) { print_puzzle(puzzle); }
 
-    size_t estimated_excluded_move_count{1500};
+    const size_t estimated_excluded_move_count{1500};
 
     std::unordered_set<Move> excluded_moves{};
     excluded_moves.reserve(estimated_excluded_move_count);
