090ba7f9b76a57eec3f0885d47a835f93bd89071
David Jordan <le_dangereux@hotmail.co.uk>
2024-03-25 20:34:58 +0000

    Change solver::print_puzzle to optionally take a stream and a callback

--- include/ball_sort/solver.hpp
+++ include/ball_sort/solver.hpp
@@ -4,6 +4,8 @@
 
 namespace solver {
 
+using ClearCallback = std::function<void()>;
+
 void solve(Puzzle& puzzle, bool display = false);
 
 [[nodiscard]] std::vector<Move>
@@ -11,7 +13,15 @@ generate_filtered_moves(const Puzzle& puzzle,
                         const std::unordered_set<Move>& excluded_moves);
 
 [[nodiscard]] Move pick_move(const std::vector<Move>& filtered_moves);
+
+void print_puzzle(
+    const Puzzle& puzzle,
+    std::ostream& output_stream,
+    const ClearCallback& clear_callback = []() {});
+
 void print_puzzle(const Puzzle& puzzle);
 void play_solution(Puzzle& puzzle, size_t moves_per_second);
+
+void clear_screen();
 
 } // namespace solver
