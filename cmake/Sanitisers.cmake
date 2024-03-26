cdb615078a09e60110ebfe2eaa553b060c5503d3
David Jordan <le_dangereux@hotmail.co.uk>
2024-03-26 23:38:11 +0000

    Break up CMake sanitiser function and disable for tests due to weird bug
    There seems to be a bug that causes the address sanitiser to be triggered by an empty test (from Google Test) or sometimes from a test with a test suite name that differs by one character from another test. Investigation is ongoing.

--- cmake/Sanitisers.cmake
+++ cmake/Sanitisers.cmake
@@ -1,61 +1,95 @@
 # -----------------------------------------------------------------------------
-# add_sanitiser_flags()
+# target_add_address_sanitiser(<target>)
 # -----------------------------------------------------------------------------
-function(add_sanitiser_flags)
-    if(NOT ENABLE_SANITISE_ADDRESS
-       AND NOT ENABLE_SANITISE_THREAD
-       AND NOT ENABLE_SANITISE_UNDEFINED)
+function(target_add_address_sanitiser TARGET)
+    if(NOT ENABLE_SANITISE_ADDRESS)
         return()
     endif()
 
     if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES
                                                 "GNU")
+        target_compile_options(${TARGET} PRIVATE "-fno-omit-frame-pointer")
+        target_link_options(${TARGET} PRIVATE "-fno-omit-frame-pointer")
 
-        add_compile_options("-fno-omit-frame-pointer")
-        add_link_options("-fno-omit-frame-pointer")
-
-        if(ENABLE_SANITISE_ADDRESS)
-            message(STATUS "[SANITISERS] Address sanitiser active")
-            add_compile_options("-fsanitize=address")
-            add_link_options("-fsanitize=address")
-        endif()
-
-        if(ENABLE_SANITISE_THREAD)
-            if(ENABLE_SANITISE_ADDRESS)
-                message(
-                    WARNING
-                        "Thread sanitiser does not work with address sanitiser")
-            else()
-                message(STATUS "[SANITISERS] Thread sanitiser active")
-                add_compile_options("-fsanitize=thread")
-                add_link_options("-fsanitize=thread")
-            endif()
-        endif()
-
-        if(ENABLE_SANITISE_UNDEFINED)
-            message(STATUS "[SANITISERS] Undefined behaviour sanitiser active")
-            add_compile_options("-fsanitize=undefined")
-            add_link_options("-fsanitize=undefined")
-        endif()
+        message(
+            STATUS "[SANITISERS] Address sanitiser active for target: ${TARGET}"
+        )
+        target_compile_options(${TARGET} PRIVATE "-fsanitize=address")
+        target_link_options(${TARGET} PRIVATE "-fsanitize=address")
 
     elseif(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
-
-        if(ENABLE_SANITISE_ADDRESS)
-            message(STATUS "[SANITISERS] Address sanitiser active")
-            add_compile_options("/fsanitize=address")
-        endif()
-
-        if(ENABLE_SANITISE_UNDEFINED)
-            message(
-                WARNING "Undefined behaviour sanitiser not implemented for MSVC"
-            )
-        endif()
-
-        if(ENABLE_SANITISE_THREAD)
-            message(WARNING "Thread sanitiser not implemented for MSVC")
-        endif()
+        message(
+            STATUS "[SANITISERS] Address sanitiser active for target: ${TARGET}"
+        )
+        target_add_compile_options(${TARGET} PRIVATE "/fsanitize=address")
 
     else()
         message(WARNING "Compiler not supported for sanitisers")
+
+    endif()
+
+endfunction()
+
+# -----------------------------------------------------------------------------
+# target_add_undefined_behaviour_sanitiser()
+# -----------------------------------------------------------------------------
+function(target_add_undefined_behaviour_sanitiser TARGET)
+    if(NOT ENABLE_SANITISE_UNDEFINED)
+        return()
+    endif()
+
+    if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES
+                                                "GNU")
+        target_compile_options(${TARGET} PRIVATE "-fno-omit-frame-pointer")
+        target_link_options(${TARGET} PRIVATE "-fno-omit-frame-pointer")
+
+        message(
+            STATUS
+                "[SANITISERS] Undefined behaviour sanitiser active for target: ${TARGET}"
+        )
+        target_compile_options(${TARGET} PRIVATE "-fsanitize=undefined")
+        target_link_options(${TARGET} PRIVATE "-fsanitize=undefined")
+
+    elseif(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+        message(
+            WARNING "Undefined behaviour sanitiser not implemented for MSVC")
+
+    else()
+        message(WARNING "Compiler not supported for sanitisers")
+
+    endif()
+endfunction()
+
+# -----------------------------------------------------------------------------
+# target_add_thread_sanitiser(<target>)
+# -----------------------------------------------------------------------------
+function(target_add_thread_sanitiser TARGET)
+    if(NOT ENABLE_SANITISE_THREAD)
+        return()
+    endif()
+
+    if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES
+                                                "GNU")
+        target_compile_options(${TARGET} PRIVATE "-fno-omit-frame-pointer")
+        target_link_options(${TARGET} PRIVATE "-fno-omit-frame-pointer")
+
+        if(ENABLE_SANITISE_ADDRESS)
+            message(
+                WARNING "Thread sanitiser does not work with address sanitiser")
+        else()
+            message(
+                STATUS
+                    "[SANITISERS] Thread sanitiser active for target: ${TARGET}"
+            )
+            add_compile_options("-fsanitize=thread")
+            add_link_options("-fsanitize=thread")
+        endif()
+
+    elseif(CMAKE_CXX_COMPILER_ID MATCHES PRIVATE "MSVC")
+        message(WARNING "Thread sanitiser not implemented for MSVC")
+
+    else()
+        message(WARNING "Compiler not supported for sanitisers")
+
     endif()
 endfunction()
