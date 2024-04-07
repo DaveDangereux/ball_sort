0cb1bf7afc0c95d8684aca3d50bb2528df513c87
David Jordan <le_dangereux@hotmail.co.uk>
2024-04-07 20:18:12 +0100

    Remove ccache dependency from Windows GitHub action

--- scripts/windows/debug_test.ps1
+++ scripts/windows/debug_test.ps1
@@ -1,9 +1,4 @@
-if (-not (Test-Path $(Get-Command ccache -ErrorAction SilentlyContinue))) {
-    Write-Error "ccache is required but it's not installed. Aborting."
-    exit 1
-}
-
 Remove-Item -Recurse -Force build, bin -ErrorAction SilentlyContinue
-cmake -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+cmake -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
 .\bin\Debug\ball_sort_tests.exe
