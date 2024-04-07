0f1d448614f776428cad1c294873926918cd8329
David Jordan <le_dangereux@hotmail.co.uk>
2024-04-07 21:55:27 +0100

    Add dir command to further investigate build error

--- scripts/windows/debug_test.ps1
+++ scripts/windows/debug_test.ps1
@@ -1,4 +1,6 @@
 Remove-Item -Recurse -Force build, bin -ErrorAction SilentlyContinue
 cmake -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
+dir bin
+dir bin\Debug
 # .\bin\Debug\ball_sort_tests.exe
