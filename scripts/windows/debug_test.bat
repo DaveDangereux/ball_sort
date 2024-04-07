ce099830b9d0daa7633e6db44b316e3094db00b3
David Jordan <le_dangereux@hotmail.co.uk>
2024-04-07 22:31:37 +0100

    The efforts continue

--- scripts/windows/debug_test.bat
+++ scripts/windows/debug_test.bat
@@ -1,4 +1,5 @@
-Remove-Item -Recurse -Force build, bin -ErrorAction SilentlyContinue
+rmdir /s /q ./bin
+rmdir /s /q ./build
 cmake -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
 ./bin/Debug/ball_sort_tests.exe
