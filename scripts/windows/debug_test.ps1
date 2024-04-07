de44d156b85bc0a340b2ab6d72d33e1f81d2de89
David Jordan <le_dangereux@hotmail.co.uk>
2024-04-07 21:50:17 +0100

    Try not executing Windows binary to deduce cause of build error

--- scripts/windows/debug_test.ps1
+++ scripts/windows/debug_test.ps1
@@ -1,4 +1,4 @@
 Remove-Item -Recurse -Force build, bin -ErrorAction SilentlyContinue
 cmake -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
-.\bin\Debug\ball_sort_tests.exe
+# .\bin\Debug\ball_sort_tests.exe
