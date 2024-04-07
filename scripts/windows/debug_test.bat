6c4980882a9404d56442d0e890f44f182d6c3efa
David Jordan <le_dangereux@hotmail.co.uk>
2024-04-07 22:50:44 +0100

    And and continue

--- scripts/windows/debug_test.bat
+++
@@ -1,5 +0,0 @@
-rmdir /s /q .\bin
-rmdir /s /q .\build
-cmake -B build -DCMAKE_BUILD_TYPE=Debug
-cmake --build build
-.\bin\Debug\ball_sort_tests.exe
---
+++ scripts/windows/debug_test.ps1
@@ -0,0 +1,4 @@
+Remove-Item -Recurse -Force build, bin -ErrorAction SilentlyContinue
+cmake -B build -DCMAKE_BUILD_TYPE=Debug
+cmake --build build
+./bin/Debug/ball_sort_tests.exe -RunAsAdmin
