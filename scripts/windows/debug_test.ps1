84ea03d0d4c1847bebd5b4324c8bb6b1cc76c050
David Jordan <le_dangereux@hotmail.co.uk>
2024-04-07 22:01:39 +0100

    Trying to get PowerShell to launch an executable in a subdirectory

--- scripts/windows/debug_test.ps1
+++ scripts/windows/debug_test.ps1
@@ -1,6 +1,4 @@
 Remove-Item -Recurse -Force build, bin -ErrorAction SilentlyContinue
 cmake -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
-dir bin
-dir bin\Debug
-# .\bin\Debug\ball_sort_tests.exe
+& .\bin\Debug\ball_sort_tests.exe
