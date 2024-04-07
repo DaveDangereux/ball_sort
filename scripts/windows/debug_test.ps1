cc00ef45947b6bd5519e7d718f3ebf7724f80d79
David Jordan <le_dangereux@hotmail.co.uk>
2024-04-07 22:20:27 +0100

    Yet another PowerShell attempt

--- scripts/windows/debug_test.ps1
+++ scripts/windows/debug_test.ps1
@@ -1,4 +1,4 @@
 Remove-Item -Recurse -Force build, bin -ErrorAction SilentlyContinue
 cmake -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
-Start-Process -FilePath ".\bin\Debug\ball_sort_tests.exe" -Verb RunAs
+./bin/Debug/ball_sort_tests.exe
