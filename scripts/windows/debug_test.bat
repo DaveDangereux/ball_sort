7067e25f7b4db921642199b302e38370b45dba06
David Jordan <le_dangereux@hotmail.co.uk>
2024-04-07 22:34:32 +0100

    And continue

--- scripts/windows/debug_test.bat
+++ scripts/windows/debug_test.bat
@@ -1,5 +1,5 @@
-rmdir /s /q ./bin
-rmdir /s /q ./build
+rmdir /s /q .\bin
+rmdir /s /q .\build
 cmake -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
-./bin/Debug/ball_sort_tests.exe
+.\bin\Debug\ball_sort_tests.exe
