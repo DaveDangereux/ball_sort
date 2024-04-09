7f13470353ba684e87dbb4a378bbf31161549217
David Jordan <le_dangereux@hotmail.co.uk>
2024-04-09 22:51:30 +0100

    Woooooooooooooooooooof

--- scripts/windows/release.bat
+++ scripts/windows/release.bat
@@ -1,4 +1,10 @@
+where sccache >nul 2>&1
+if errorlevel 1 (
+    echo sccache is required but it's not installed. Aborting.
+    exit /b 1
+)
+
 rmdir /S /Q build
 rmdir /S /Q bin
-cmake -B build -DCMAKE_BUILD_TYPE=Release -DENABLE_TESTS=OFF
+cmake -B build -DCMAKE_BUILD_TYPE=Release -DENABLE_TESTS=OFF -DCMAKE_CXX_COMPILER_LAUNCHER=sccache
 cmake --build build
--- scripts/windows/release_test.bat
+++ scripts/windows/release_test.bat
@@ -1,5 +1,11 @@
+where sccache >nul 2>&1
+if errorlevel 1 (
+    echo sccache is required but it's not installed. Aborting.
+    exit /b 1
+)
+
 rmdir /S /Q build
 rmdir /S /Q bin
-cmake -B build -DCMAKE_BUILD_TYPE=Release
+cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER_LAUNCHER=sccache
 cmake --build build
-bin\Debug\ball_sort_tests.exe
+bin\Release\ball_sort_tests.exe
