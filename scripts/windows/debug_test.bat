b1e2e9750d82fab5cf389f67bf3822f10e6b9a2f
David Jordan <le_dangereux@hotmail.co.uk>
2024-04-09 19:22:00 +0100

    Meow

--- scripts/windows/debug_test.bat
+++ scripts/windows/debug_test.bat
@@ -1,5 +1,5 @@
-rmdir -r -q build
-rmdir -r -q bin
+rmdir /S /Q build
+rmdir /S /Q bin
 cmake -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
-bin\Debug\ball_sort_tests
+bin\Debug\ball_sort_tests.exe
