b82b4d3be2d922459bdeaf97202f7f7e6a30d12e
Dave Jordan <61416292+DaveDangereux@users.noreply.github.com>
2024-04-09 22:57:46 +0100

    Add GitHub Actions (DT-93)

---
+++ scripts/windows/debug_test.bat
@@ -0,0 +1,11 @@
+where sccache >nul 2>&1
+if errorlevel 1 (
+    echo sccache is required but it's not installed. Aborting.
+    exit /b 1
+)
+
+rmdir /S /Q build
+rmdir /S /Q bin
+cmake -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER_LAUNCHER=sccache
+cmake --build build
+bin\Debug\ball_sort_tests.exe
